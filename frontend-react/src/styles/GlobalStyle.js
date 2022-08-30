df0439b62fe4176232024a301e7769353070f613
Nadim <nahlenadim@gmail.com>
2022-08-30 09:46:25 +0300

    Frontend-React Install Fonts

---
+++ frontend-react/src/styles/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+
+
+
+const GlobalStyles = createGlobalStyle`
+
+*,*::before,*::after{
+    margin: 0;
+    padding: 0;
+}
+
+body{
+    font-family: ;
+}
+
+
+`
