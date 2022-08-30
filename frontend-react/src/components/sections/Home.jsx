61b959693ecedc182e48cc954ad00bd11db879c8
Nadim <nahlenadim@gmail.com>
2022-08-30 12:39:29 +0300

    Frontend-React Create- Container in Home

--- frontend-react/src/components/sections/Home.jsx
+++ frontend-react/src/components/sections/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: ${(props) => `calc(100vh - ${props.theme.navHeight})`};
+  width: 100vw;
+  position: relative;
+  background-color: ${(props) => props.theme.navHeight});
+`;
+
+const Container = styled.div`
+  width: 75%;
+  min-height: 80vh;
+  margin: 0 auto;
+  background-color: lightblue;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Section>
+      <Container>
+        <h1>text</h1>
+        <h1>video</h1>
+      </Container>
+    </Section>
+  );
 };
 
 export default Home;
