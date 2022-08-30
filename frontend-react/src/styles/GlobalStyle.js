6023430059adc0f45769f16476da5311b47fa1da
Nadim <nahlenadim@gmail.com>
2022-08-30 09:49:13 +0300

    Frontend-React Create- Global Style

--- frontend-react/src/styles/GlobalStyle.js
+++ frontend-react/src/styles/GlobalStyle.js
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from "styled-components";
+import "@fontsource/akaya-telivigala"
+import "@fontsource/sora"
 
 
 
@@ -11,8 +13,20 @@ const GlobalStyles = createGlobalStyle`
 }
 
 body{
-    font-family: ;
+    font-family: 'sora', sans-serif;
+    overflow-x: hidden;
 }
 
+h1,h2,h3,h4,h5,h6{
+    margin: 0;
+    padding: 0;
+}
+
+a{
+    color: inherit;
+    text-decoration:none;
+}
 
 `
+
+export default GlobalStyles;
