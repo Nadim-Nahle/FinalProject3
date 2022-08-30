2a495e661140211d6b5630d7a8948875bd202a95
Nadim <nahlenadim@gmail.com>
2022-08-30 13:02:50 +0300

    Frontend-React Add- Colors to Type Writer

--- frontend-react/src/components/TypeWriterText.jsx
+++ frontend-react/src/components/TypeWriterText.jsx
@@ -5,21 +5,43 @@ import Typewriter from "typewriter-effect";
 const Title = styled.h2`
   font-size: ${(props) => props.theme.fontxx1};
   text-transform: capitalize;
+  width: 80%;
+  color: ${(props) => props.theme.text};
+  align-self: flex-start;
+
+  span {
+    text-transform: uppercase;
+    font-family: "Akaya Telivigala", cursive;
+  }
+
+  .text-1 {
+    color: blue;
+  }
+  .text-2 {
+    color: orange;
+  }
+  .text-3 {
+    color: red;
+  }
 `;
 const TypeWriterText = () => {
   return (
     <Title>
       Discover a new era of cool
       <Typewriter
+        options={{
+          autoStart: true,
+          loop: true,
+        }}
         onInit={(typewriter) => {
           typewriter
-            .typeString("NFTs.")
+            .typeString('<span class="text-1">NFTs.</span>')
             .pauseFor(2000)
             .deleteAll()
-            .typeString("Collectible Items.")
+            .typeString('<span class="text-2">"Collectible Items."</span>')
             .pauseFor(2000)
             .deleteAll()
-            .typeString("Ape Killers!")
+            .typeString('<span class="text-3">"Ape Killers!"</span>')
             .pauseFor(2000)
             .deleteAll()
             .start();
