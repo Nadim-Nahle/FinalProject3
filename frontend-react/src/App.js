66137c422e225246d7364214f4f6b1ad40754a2f
Nadim <nahlenadim@gmail.com>
2022-08-30 10:13:42 +0300

    Frontend-React Add Components to app.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,7 +1,14 @@
 import { ThemeProvider } from "styled-components";
-import { Navigation } from "swiper";
+import Navigation from './components/Navigation';
 import GlobalStyles from "./styles/GlobalStyle";
 import { light } from './styles/Themes'
+import Home from './components/sections/Home'
+import About from './components/sections/About'
+import Roadmap from './components/sections/Roadmap'
+import Showcase from './components/sections/Home'
+import Team from './components/sections/Team'
+import Faq from './components/sections/Faq'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +16,13 @@ function App() {
       <GlobalStyles />
       <ThemeProvider theme={light}>
         <Navigation />
+        <Home />
+        <About />
+        <Roadmap />
+        <Showcase />
+        <Team />
+        <Faq />
+        <Footer />
       </ThemeProvider>
     </>
   );
