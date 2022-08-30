39b2e1f7f4c71eebaa40e2d8d966adcda84167a1
Nadim <nahlenadim@gmail.com>
2022-08-30 11:59:02 +0300

    Frontend-React Add Hover Effect to Menu Item

--- frontend-react/src/components/Navigation.jsx
+++ frontend-react/src/components/Navigation.jsx
@@ -28,6 +28,19 @@ const MenuItem = styled.li`
   margin: 0 1rem;
   color: ${(props) => props.theme.text};
   cursor: pointer;
+
+  &::after {
+    content: " ";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${(props) => props.theme.text};
+    transition: width 0.3s ease;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
 `;
 
 const Navigation = () => {
