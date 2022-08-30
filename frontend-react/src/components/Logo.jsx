b284d1fc733f0436effa9798297c68ae5d91505a
Nadim <nahlenadim@gmail.com>
2022-08-30 11:47:30 +0300

    Frontend-React Fix- Font

--- frontend-react/src/components/Logo.jsx
+++ frontend-react/src/components/Logo.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const LogoText = styled.h1`
   font-family: "Akaya Telivigala", cursive;
   font-size: ${(props) => props.theme.fontxxx1};
+  color: ${(props) => props.theme.text};
 `;
 
 const Logo = () => {
-  return <LogoText>w.</LogoText>;
+  return (
+    <LogoText>
+      <Link to="/">W.</Link>{" "}
+    </LogoText>
+  );
 };
 
 export default Logo;
