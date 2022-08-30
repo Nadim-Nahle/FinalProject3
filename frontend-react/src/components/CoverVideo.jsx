be6e5adb3d1fda1c6af20f67c2ce6855fced2b8e
Nadim <nahlenadim@gmail.com>
2022-08-30 13:28:58 +0300

    Frontend-React Create- Video Container

--- frontend-react/src/components/CoverVideo.jsx
+++ frontend-react/src/components/CoverVideo.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import GIF from "../assets/Home Video.mp4";
+
+const VideoContaier = styled.div`
+  width: 100%;
+
+  video {
+    width: 100%;
+    height: auto;
+  }
+`;
 
 const CoverVideo = () => {
-  return <div>CoverVideo</div>;
+  return (
+    <VideoContaier>
+      <video src={GIF} type="video/mp4" autoPlay muted loop />
+    </VideoContaier>
+  );
 };
 
 export default CoverVideo;
