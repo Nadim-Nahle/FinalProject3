ff90334c91c416d50cb4684a7a60bf2ffde4a2ef
Nadim <nahlenadim@gmail.com>
2022-08-31 17:57:36 +0300

    Frontend-React Showcase- Import NFTs

--- frontend-react/src/components/sections/Showcase.jsx
+++ frontend-react/src/components/sections/Showcase.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 
+import img1 from "../../assets/Nfts/1.svg";
+import img2 from "../../assets/Nfts/2.svg";
+import img3 from "../../assets/Nfts/3.svg";
+import img4 from "../../assets/Nfts/4.svg";
+import img5 from "../../assets/Nfts/5.svg";
+import img6 from "../../assets/Nfts/6.svg";
+import img7 from "../../assets/Nfts/7.svg";
+import img8 from "../../assets/Nfts/8.svg";
+import img9 from "../../assets/Nfts/9.svg";
+import img10 from "../../assets/Nfts/10.svg";
+import img11 from "../../assets/Nfts/11.svg";
+import img12 from "../../assets/Nfts/12.svg";
+import img13 from "../../assets/Nfts/13.svg";
+
 const Section = styled.section`
   min-height: 100vh;
   width: 100vw;
@@ -17,32 +31,25 @@ const move = keyframes`
   100%{ transform: translateX(-100%)}
 `;
 const Row = styled.div`
-  background-color: lightblue;
   white-space: nowrap;
   box-sizing: content-box;
   margin: 2rem 0;
   display: flex;
 
   animation: ${move} 20s linear infinite ${(props) => props.direction};
-  div {
-    width: 5rem;
-    height: 5rem;
-    margin: 2rem;
-    background-color: red;
-  }
 `;
 
 const ImgContainer = styled.div`
   width: 15rem;
-  margin: 0 auto;
+  margin: 0 1rem;
   background-color: ${(props) => props.theme.body};
-
+  height: 50%;
   border-radius: 20px;
   cursor: pointer;
 
   img {
     width: 100%;
-    height: auto;
+    height: 10rem;
   }
 `;
 
@@ -58,20 +65,18 @@ const Showcase = () => {
   return (
     <Section>
       <Row direction="none">
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 4</div>
-        <div>item 5</div>
-        <div>item 6</div>
+        <NftItem img={img1} />
+        <NftItem img={img2} />
+        <NftItem img={img3} />
+        <NftItem img={img4} />
+        <NftItem img={img5} />
       </Row>
       <Row direction="reverse">
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 4</div>
-        <div>item 5</div>
-        <div>item 6</div>
+        <NftItem img={img1} />
+        <NftItem img={img2} />
+        <NftItem img={img3} />
+        <NftItem img={img4} />
+        <NftItem img={img5} />
       </Row>
     </Section>
   );
