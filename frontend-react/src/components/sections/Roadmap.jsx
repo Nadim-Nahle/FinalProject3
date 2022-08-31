e01d285b8b05ebc106c4c163cc86ad522b48b27f
Nadim <nahlenadim@gmail.com>
2022-08-31 12:48:25 +0300

    Frontend-React Create Rodmap Section

--- frontend-react/src/components/sections/Roadmap.jsx
+++ frontend-react/src/components/sections/Roadmap.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+min-height: 100vh;
+width: 100vw;
+background-color: ${(props) => props.theme.body}
+position: relative;
+
+`;
 
 const Roadmap = () => {
-  return <div>Roadmap</div>;
+  return <Section></Section>;
 };
 
 export default Roadmap;
