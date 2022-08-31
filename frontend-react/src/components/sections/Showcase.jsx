bbf332ea00d0860cd3c44699dbb7a2e365ffb094
Nadim <nahlenadim@gmail.com>
2022-08-31 18:07:42 +0300

    Frontend-React Showcase- Create Details

--- frontend-react/src/components/sections/Showcase.jsx
+++ frontend-react/src/components/sections/Showcase.jsx
@@ -53,10 +53,46 @@ const ImgContainer = styled.div`
   }
 `;
 
+const Details = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.8rem 1rem;
+  background-color: ${(props) => props.theme.text};
+  border: 2px solid ${(props) => `rgba(${props.theme.bodyRgba},0.5)`};
+
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+
+  span {
+    font-size: ${(props) => props.theme.fontsm};
+    color: ${(props) => `rgba(${props.theme.bodyRgba},0.5)`};
+    font-weight: 600;
+    line-height: 1.5rem;
+  }
+
+  h1 {
+    font-size: ${(props) => props.theme.fontmd};
+    color: ${(props) => props.theme.body};
+    font-weight: 600;
+  }
+`;
+
 const NftItem = ({ img, number = 0, price = 0 }) => {
   return (
     <ImgContainer>
       <img src={img} alt="World Cup" />
+      <Details>
+        <div>
+          <span>Footballers</span>
+          <br />
+          <h1>#{number}</h1>
+        </div>
+        <div>
+          <span>Price</span>
+          <br />
+          <h1>{Number(price).toFixed(1)}</h1>
+        </div>
+      </Details>
     </ImgContainer>
   );
 };
