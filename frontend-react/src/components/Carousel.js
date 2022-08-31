7eeaee6ec379750e79c5ef85444aa36e992c7ee4
Nadim <nahlenadim@gmail.com>
2022-08-31 10:43:15 +0300

    Frontend-React Import Swipe

--- frontend-react/src/components/Carousel.js
+++ frontend-react/src/components/Carousel.js
@@ -5,9 +5,29 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 import { EffectCards } from "swiper";
 
+import img1 from '../assets/Nfts/1.svg'
+import img2 from '../assets/Nfts/2.svg'
+import img3 from '../assets/Nfts/3.svg'
+import img4 from '../assets/Nfts/4.svg'
+import img5 from '../assets/Nfts/5.svg'
+import img6 from '../assets/Nfts/6.svg'
+import img7 from '../assets/Nfts/7.svg'
+import img8 from '../assets/Nfts/8.svg'
+import img9 from '../assets/Nfts/9.svg'
+import img10 from '../assets/Nfts/10.svg'
+import img11 from '../assets/Nfts/11.svg'
+import img12 from '../assets/Nfts/12.svg'
+import img13 from '../assets/Nfts/13.svg'
+
 const Container = styled.div`
 width: 25vw;
 height: 70vh;
+
+
+.swiper{
+    width:100%;
+    height: 70vh;
+}
 `
 
 const Carousel = () => {
