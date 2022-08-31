8b08d262ef314ee353613169bb6122345d03a6be
Nadim <nahlenadim@gmail.com>
2022-08-31 10:28:24 +0300

    Frontend-React Add- Section to ABout

--- frontend-react/src/components/sections/About.jsx
+++ frontend-react/src/components/sections/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+`;
 
 const About = () => {
-  return <div>About</div>;
+  return <Section>About</Section>;
 };
 
 export default About;
