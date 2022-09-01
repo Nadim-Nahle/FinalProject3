1a3e41cbd51aaef2345d99943daf02cef3a7601b
Nadim <nahlenadim@gmail.com>
2022-09-01 12:01:21 +0300

    Frontend-React Team- Add MyConfetti props

--- frontend-react/src/components/MyConfetti.jsx
+++ frontend-react/src/components/MyConfetti.jsx
@@ -3,7 +3,15 @@ import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
 
 const MyConfetti = () => {
-  return <div>Confetti</div>;
+  const { width, height } = useWindowSize();
+  return (
+    <Confetti
+      numberOfPieces={150}
+      gravity={0.01}
+      width={width}
+      height={height * 1.5}
+    />
+  );
 };
 
 export default MyConfetti;
