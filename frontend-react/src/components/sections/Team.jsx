586d0ae9af154ccdbeffb494122f9864d12524b6
Nadim <nahlenadim@gmail.com>
2022-09-01 11:26:30 +0300

    Frontend-React Team- Import NFTs

--- frontend-react/src/components/sections/Team.jsx
+++ frontend-react/src/components/sections/Team.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
+import img1 from "../../assets/Nfts/1.svg";
+import img2 from "../../assets/Nfts/2.svg";
+import img3 from "../../assets/Nfts/3.svg";
+import img4 from "../../assets/Nfts/4.svg";
+import img5 from "../../assets/Nfts/5.svg";
+import img6 from "../../assets/Nfts/6.svg";
+import img7 from "../../assets/Nfts/7.svg";
+import img8 from "../../assets/Nfts/8.svg";
+import img9 from "../../assets/Nfts/9.svg";
+import img10 from "../../assets/Nfts/10.svg";
+import img11 from "../../assets/Nfts/11.svg";
+import img12 from "../../assets/Nfts/12.svg";
+
 const Section = styled.section`
   min-height: 100vh;
   width: 100vw;
