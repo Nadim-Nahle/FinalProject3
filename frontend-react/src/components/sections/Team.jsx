30e460e1bed734b3b6436c015c7005f0c60f6cd5
Nadim <nahlenadim@gmail.com>
2022-09-01 11:18:13 +0300

    Frontend-React Team- Create Title

--- frontend-react/src/components/sections/Team.jsx
+++ frontend-react/src/components/sections/Team.jsx
@@ -8,6 +8,17 @@ const Section = styled.section`
   position: relative;
 `;
 
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+`;
 const Team = () => {
   return <div>Team</div>;
 };
