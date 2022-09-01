77f60d919280ff52abac664313eff6edcb082597
Nadim <nahlenadim@gmail.com>
2022-09-01 07:35:20 +0300

    Frontend-React Accorfion- Create Section

--- frontend-react/src/components/Accordion.jsx
+++ frontend-react/src/components/Accordion.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Accordion = () => {
-  return <div>Accordion</div>;
+const Container = styled.div`
+  cursor: pointer;
+  padding: 1rem 0.5rem;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid ${(props) => props.theme.carouselColor};
+  margin: 3rem 0;
+`;
+const Accordion = ({ title, children }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Reveal>{children}</Reveal>
+    </Container>
+  );
 };
 
 export default Accordion;
