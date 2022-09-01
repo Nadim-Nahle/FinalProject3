d719e5ef538fa153d725ac933a39053d448cc7c4
Nadim <nahlenadim@gmail.com>
2022-09-01 07:24:33 +0300

    Frontend-React FAQ- Create Faq

--- frontend-react/src/components/sections/Faq.jsx
+++ frontend-react/src/components/sections/Faq.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.text};
+  position: relative;
+`;
 
 const Faq = () => {
   return <div>Faq</div>;
