33cc48969142e6918dbf5732c51805c5a0210351
Nadim <nahlenadim@gmail.com>
2022-09-01 11:17:40 +0300

    Frontend-React Team- Create section

--- frontend-react/src/components/sections/Team.jsx
+++ frontend-react/src/components/sections/Team.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+  position: relative;
+`;
 
 const Team = () => {
   return <div>Team</div>;
