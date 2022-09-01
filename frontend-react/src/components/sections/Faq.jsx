6ba2064110436ed72d7ec7bec47d985225d8a7af
Nadim <nahlenadim@gmail.com>
2022-09-01 08:19:33 +0300

    Frontend-React FAQ- Add Content

--- frontend-react/src/components/sections/Faq.jsx
+++ frontend-react/src/components/sections/Faq.jsx
@@ -47,27 +47,41 @@ const Faq = () => {
             Once minted or bought simply connect to your OpenSea account to view
             your NFTs.
           </Accordion>
-          <Accordion title="WHERE CAN I VIEW MY NFTS?">
-            Once minted or bought simply connect to your OpenSea account to view
-            your NFTs.
+          <Accordion
+            title="WHAT IS THE METAVERSE?
+"
+          >
+            A metaverse is a network of 3D virtual worlds focused on social
+            connection. In futurism and science fiction, it is often described
+            as a hypothetical iteration of the Internet as a single, universal
+            virtual world that is facilitated by the use of virtual and
+            augmented reality headsets.
           </Accordion>
-          <Accordion title="WHERE CAN I VIEW MY NFTS?">
-            Once minted or bought simply connect to your OpenSea account to view
-            your NFTs.
+          <Accordion title="WHY DO WE NEED ROYALTIES?">
+            The amount of royalties was fixed at 5% to finance the World Cup
+            Club's projects. We have the ambition to organize multiple events
+            around the world in order to strengthen the community and build a
+            network of entrepreneurs and investors with the same mindset and
+            common interests.
           </Accordion>
         </Box>
         <Box>
-          <Accordion title="WHERE CAN I VIEW MY NFTS?">
-            Once minted or bought simply connect to your OpenSea account to view
-            your NFTs.
+          <Accordion title="HOW CAN I USE MY NFT?">
+            You will be able to use your NFT as an avatar in the Metaverse and
+            our future video game. Holding also means that you are part of an
+            exclusive network of investors and entrepreneurs.
           </Accordion>
-          <Accordion title="WHERE CAN I VIEW MY NFTS?">
-            Once minted or bought simply connect to your OpenSea account to view
-            your NFTs.
+          <Accordion title="WHAT ARE THE Footballers?">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel enim
+            veritatis iusto officia. Exercitationem, ducimus reiciendis. Rem,
+            maxime, similique neque minus aliquam dolore doloremque laboriosam,
+            facilis quibusdam unde sint officia..
           </Accordion>
-          <Accordion title="WHERE CAN I VIEW MY NFTS?">
-            Once minted or bought simply connect to your OpenSea account to view
-            your NFTs.
+          <Accordion title="WHAT IS THE FUSION PROCESS?">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel enim
+            veritatis iusto officia. Exercitationem, ducimus reiciendis. Rem,
+            maxime, similique neque minus aliquam dolore doloremque laboriosam,
+            facilis quibusdam unde sint officia.
           </Accordion>
         </Box>
       </Container>
