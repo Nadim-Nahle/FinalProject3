38256c473375a26f80a48c56a2e7d9271f61ad89
Nadim <nahlenadim@gmail.com>
2022-09-02 13:09:02 +0300

    Frontend-React TypeWriter- Add Media Query

--- frontend-react/src/components/TypeWriterText.jsx
+++ frontend-react/src/components/TypeWriterText.jsx
@@ -31,6 +31,9 @@ const Title = styled.h2`
     align-self: center;
     text-align: center;
   }
+  @media (max-width: 40em) {
+    width: 90%;
+  }
 `;
 
 const SubTitle = styled.h3`
@@ -44,11 +47,25 @@ const SubTitle = styled.h3`
   @media (max-width: 40em) {
     font-size: ${(props) => props.theme.fontmd};
   }
+
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+  }
 `;
 
 const ButtonContainer = styled.div`
   width: 80%;
   align-self: flex-start;
+
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+
+    button {
+      margin: 0 auto;
+    }
+  }
 `;
 
 const TypeWriterText = () => {
