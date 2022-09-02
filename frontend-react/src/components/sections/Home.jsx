a5fd87d3992880e0542f144b22d2bdb36cb5d92c
Nadim <nahlenadim@gmail.com>
2022-09-02 13:17:15 +0300

    Frontend-React Home- Add Media Query

--- frontend-react/src/components/sections/Home.jsx
+++ frontend-react/src/components/sections/Home.jsx
@@ -61,6 +61,16 @@ const Round = styled.div`
     height: auto;
     animation: ${rotate} 6s linear infinite reverse;
   }
+  @media (max-width: 64em) {
+    width: 4rem;
+    height: 4rem;
+    left: none;
+    right: 2rem;
+    bottom: 100%;
+  }
+  @media (max-width: 48em) {
+    right: 1rem;
+  }
 `;
 
 const Circle = styled.span`
@@ -79,6 +89,11 @@ const Circle = styled.span`
   background-color: ${(props) => props.theme.text};
   color: ${(props) => props.theme.body};
   font-size: ${(props) => props.theme.fontxl};
+  @media (max-width: 64em) {
+    width: 2rem;
+    height: 2rem;
+    font-size: ${(props) => props.theme.fontxlg};
+  }
 `;
 
 const Home = () => {
