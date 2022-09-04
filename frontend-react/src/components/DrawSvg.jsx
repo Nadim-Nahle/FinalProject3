e1c851f780dc3ac74a9214bba6d91009f68ecad0
Nadim <nahlenadim@gmail.com>
2022-09-04 09:06:34 +0300

    Frontend-React DrawSvg- Add Media Query

--- frontend-react/src/components/DrawSvg.jsx
+++ frontend-react/src/components/DrawSvg.jsx
@@ -12,25 +12,27 @@ const VectorContainer = styled.div`
   transform: translateX(-50%);
   width: 100%;
   height: 100%;
-  overflow: hideen;
+  overflow: hidden;
   svg {
+    display: inline-block;
     width: 100%;
     height: 100%;
+  }
+
+  @media (max-width: 48em) {
+    left: 1rem;
   }
 `;
 
 const Bounce = keyframes`
-  from {
-    transform: translateX(-50%) scale(0.5);
-  }
-  to {
-    transform: translateX(-50%) scale(1);
-  }
+  from { transform: translateX(-50%) scale(0.5); }
+  to { transform: translateX(-50%) scale(1); }
+
 `;
 
 const Ball = styled.div`
   position: absolute;
-  top: 0.5rem;
+  top: 0;
   left: 50%;
   transform: translateX(-50%);
   width: 1.5rem;
@@ -38,6 +40,10 @@ const Ball = styled.div`
   border-radius: 50%;
   background-color: ${(props) => props.theme.text};
   animation: ${Bounce} 0.5s linear infinite alternate;
+
+  @media (max-width: 48em) {
+    left: 1rem;
+  }
 `;
 
 const DrawSvg = () => {
@@ -68,9 +74,9 @@ const DrawSvg = () => {
         },
         // onToggle: (self) => {
         //   if (self.isActive) {
-        //     ballRef.current.style.display = "none ;";
+        //     ballRef.current.style.display = "none";
         //   } else {
-        //     ballRef.current.style.display = "inline-block;";
+        //     ballRef.current.style.display = "inline-block";
         //   }
         // },
       },
