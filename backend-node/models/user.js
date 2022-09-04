5db00adfe150a555de1a716fdac024ed7a01cfa7
Nadim <nahlenadim@gmail.com>
2022-09-04 14:22:37 +0300

    Backend-Node Create- User Model

---
+++ backend-node/models/user.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+//USER SCHEMA
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+
+    username: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    phoneNumber: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+    country: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+    city: {
+      type: String,
+      min: 6,
+      max: 255,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      min: [6, "please choose a stronger password"],
+      max: 1024,
+    },
+
+    roles: {
+      type: [
+        {
+          type: String,
+          enum: ["user", "admin", "premium"],
+        },
+      ],
+      default: ["user"],
+    },
+
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+//EXPORTING SCHEMA
+module.exports = mongoose.model("user", userSchema);
