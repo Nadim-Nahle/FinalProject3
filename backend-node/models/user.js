2d4c15f7e7b3deb368734128a8abe6aa126381a8
Nadim <nahlenadim@gmail.com>
2022-09-05 11:04:41 +0300

    Backend-Node Add Nft refference to user

--- backend-node/models/user.js
+++ backend-node/models/user.js
@@ -97,6 +97,12 @@ const userSchema = new mongoose.Schema(
         ref: "Faq",
       },
     ],
+    nft: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Nft",
+      },
+    ],
   },
   { timestamps: true }
 );
