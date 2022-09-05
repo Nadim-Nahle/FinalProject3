dd6ad96b2b28e3ab1c65b6af9bc3c21a57bf9572
Nadim <nahlenadim@gmail.com>
2022-09-05 10:52:38 +0300

    Backend-Node Create- team Model

---
+++ backend-node/models/team.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+//MOVIE SCHEMA
+const teamSchema = new mongoose.Schema({
+
+  picture: {
+    type: String,
+    default: "",
+  },
+
+  price: {
+    type: String,
+    min: 1,
+    max: 255,
+  },
+
+  number: {
+    type: String,
+    min: 1,
+    max: 255,
+  },
+
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+});
+
+//EXPORTING SCHEMA
+module.exports = mongoose.model("Team", teamSchema);
