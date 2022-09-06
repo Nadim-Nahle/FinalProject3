6a6c88bfa402c0d865c906a4a0b25a8db46b863f
Nadim <nahlenadim@gmail.com>
2022-09-06 13:00:35 +0300

    Backend-Node Create getHome Controller

--- backend-node/controllers/HomeController.js
+++ backend-node/controllers/HomeController.js
@@ -2,14 +2,27 @@ const { all } = require("../app/routes");
 const Home = require("../models/home");
 const fs = require('fs');
 
-//ADD MOVIE CONTROLLER
+//ADD HOME CONTROLLER
 async function addHome(req, res) {
-  const movie = new Home({ ...req.body, owner: req.user._id });
+  const home = new Home({ ...req.body, owner: req.user._id });
   try {
-    await movie.save();
-    res.status(201).send(movie);
+    await home.save();
+    res.status(201).send(home);
   } catch (error) {
     res.status(500).send(error.message);
+  }
+}
+
+//GET HOME CONTROLLER
+async function getHome(req, res) {
+  try {
+    const home = await Home.find({ owner: req.user._id });
+    if (!home) {
+      return res.status(404).send();
+    }
+    res.status(200).send(home);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 }
 
