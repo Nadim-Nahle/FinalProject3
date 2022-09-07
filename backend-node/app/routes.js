a4ea6991784f811b05be290b2afefbcce8823946
Nadim <nahlenadim@gmail.com>
2022-09-07 10:31:44 +0300

    Backend-Node Create getRoadmap Rouete

--- backend-node/app/routes.js
+++ backend-node/app/routes.js
@@ -40,6 +40,7 @@ router.patch("/auth/about/update/:id", auth, updateAbout);
 
 //ROADMAP ROUTES
 router.post("/auth/addroadmap", auth, addRoadmap);
+router.get("/auth/roadmap", auth, getRoadmap);
 
 
 module.exports = router;
