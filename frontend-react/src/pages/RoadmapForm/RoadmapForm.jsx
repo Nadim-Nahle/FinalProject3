64ab9220fd17eb52e18fa4652648c6908b6acef7
Nadim <nahlenadim@gmail.com>
2022-09-09 15:15:21 +0300

    Frontend-React Create AnoutForm

---
+++ frontend-react/src/pages/RoadmapForm/RoadmapForm.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./RoadmapForm.css";
+
+const RoadmapForm = () => {
+  return (
+    <div className="container">
+      <div className="contact-box">
+        <div className="left"></div>
+        <div className="right">
+          <h2>Let's Create the Roadmap Page!</h2>
+
+          <textarea
+            placeholder="Please Enter Opening Details"
+            className="field"
+          ></textarea>
+          <textarea
+            placeholder="Please Enter Benifit Details"
+            className="field"
+          ></textarea>
+          <textarea
+            placeholder="Please Enter Early Access Details!"
+            className="field"
+          ></textarea>
+          <textarea
+            placeholder="Please Enter New Merch Details!"
+            className="field"
+          ></textarea>
+          <textarea
+            placeholder="Please Enter Holder Rankings Details!"
+            className="field"
+          ></textarea>
+          <button className="btn">Next</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RoadmapForm;
