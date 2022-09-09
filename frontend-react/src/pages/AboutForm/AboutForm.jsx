c399db62cdc47b9d4ea0c12c48b346272b1256cd
Nadim <nahlenadim@gmail.com>
2022-09-09 15:11:16 +0300

    Frontend-React Create AnoutForm

--- frontend-react/src/pages/AboutForm/AboutForm.jsx
+++ frontend-react/src/pages/AboutForm/AboutForm.jsx
@@ -8,14 +8,14 @@ const AboutForm = () => {
         <div className="left"></div>
         <div className="right">
           <h2>Let's Create the About Page!</h2>
-          <input type="text" className="field" placeholder="Website Name" />
-          <input
-            type="text"
-            className="field"
-            placeholder="Website Abbreviation"
-          />
+          <input type="text" className="field" placeholder="Title" />
+
           <textarea
-            placeholder="Please Enter A Catchphrase!"
+            placeholder="Please Enter A Description!"
+            className="field"
+          ></textarea>
+          <textarea
+            placeholder="Please Enter Any Other Details!"
             className="field"
           ></textarea>
           <button className="btn">Send</button>
