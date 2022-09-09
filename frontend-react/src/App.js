6e07b6b07f1443db7eb3fd75b5f296f1be48e466
Nadim <nahlenadim@gmail.com>
2022-09-09 10:13:12 +0300

    Frontend-React Create- NFTsWebsite component

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,14 +1,4 @@
-import { ThemeProvider } from "styled-components";
-import Navigation from './components/Navigation';
-import GlobalStyles from "./styles/GlobalStyle";
-import { light } from './styles/Themes'
-import Home from './components/sections/Home'
-import About from './components/sections/About'
-import Roadmap from './components/sections/Roadmap'
-import Showcase from './components/sections/Showcase'
-import Team from './components/sections/Team'
-import Faq from './components/sections/Faq'
-import Footer from './components/Footer'
+import NFTsWebsite from "./pages/NFTsWebsite";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
@@ -18,20 +8,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/*PUBLIC ROUTES*/}
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<NFTsWebsite />}></Route>
         </Route>
       </Routes>
-      {/* <GlobalStyles />
-      <ThemeProvider theme={light}>
-        <Navigation />
-        <Home />
-        <About />
-        <Roadmap />
-        <Showcase />
-        <Team />
-        <Faq />
-        <Footer />
-      </ThemeProvider> */}
+
     </>
   );
 }
--- frontend-react/src/pages/NFTsWebsite.jsx
+++ frontend-react/src/pages/NFTsWebsite.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import Navigation from "../components/Navigation";
+import GlobalStyles from "../styles/GlobalStyle";
+import { light } from "../styles/Themes";
+import Home from "../components/sections/Home";
+import About from "../components/sections/About";
+import Roadmap from "../components/sections/Roadmap";
+import Showcase from "../components/sections/Showcase";
+import Team from "../components/sections/Team";
+import Faq from "../components/sections/Faq";
+import Footer from "../components/Footer";
 
 const NFTsWebsite = () => {
-  return <div>NFTsWebsite</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+        <Navigation />
+        <Home />
+        <About />
+        <Roadmap />
+        <Showcase />
+        <Team />
+        <Faq />
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default NFTsWebsite;
