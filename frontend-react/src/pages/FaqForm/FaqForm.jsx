5b0f2eb4ac3c78f6f4aa09208d698f21bcb85042
Nadim <nahlenadim@gmail.com>
2022-09-09 15:21:21 +0300

    Frontend-React Create FAQ Form

---
+++ frontend-react/src/pages/FaqForm/FaqForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./FaqForm.css";
+
+const FaqForm = () => {
+  return (
+    <div className="container">
+      <div className="contact-box">
+        <div className="left"></div>
+        <div className="right">
+          <h2>Let's Create the Faq Page!</h2>
+
+          <textarea
+            placeholder="Please Enter A Question!"
+            className="field"
+          ></textarea>
+          <textarea
+            placeholder="Please Enter The Answer"
+            className="field"
+          ></textarea>
+          <button className="btn">Add Another Question</button>
+          <button className="btn">Next</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqForm;
