7c68364d7bea0c15f08bb31b397c519cb592b2d6
Nadim <nahlenadim@gmail.com>
2022-09-09 10:59:51 +0300

    Frontend-React Create- Login page

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,7 @@
 import NFTsWebsite from "./pages/NFTsWebsite";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           {/*PUBLIC ROUTES*/}
           <Route path="/" element={<NFTsWebsite />}></Route>
+
         </Route>
       </Routes>
 
