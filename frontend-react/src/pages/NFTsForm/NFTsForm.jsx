49e455e61d289b091bb3e96a7e7326978486e81a
Nadim <nahlenadim@gmail.com>
2022-09-10 10:50:05 +0300

    Frontend-React Create- NftsUpload

---
+++ frontend-react/src/pages/NFTsForm/NFTsForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NFTsForm.css";
+
+const NFTsForm = () => {
+  return (
+    <div className="container">
+      <div className="contact-box">
+        <div className="left"></div>
+        <div className="right">
+          <h2>Let's Upload the NFTs!</h2>
+          <input type="file" className="field" placeholder="Website Name" />
+          <input type="text" className="field" placeholder="NFT Number" />
+          <input type="text" className="field" placeholder="NFT Price" />
+          <button className="btn">Finish</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NFTsForm;
