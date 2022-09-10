541fd95a792e368741022845f3f1b81b8ac62d6d
Nadim <nahlenadim@gmail.com>
2022-09-10 14:24:45 +0300

    Frontend-React edit connect wallet button

--- frontend-react/src/components/Navigation.jsx
+++ frontend-react/src/components/Navigation.jsx
@@ -159,12 +159,12 @@ const Navigation = () => {
           <MenuItem onClick={() => scrollTo("faq")}>Faq</MenuItem>
           <MenuItem>
             <div className="mobile">
-              <Button text="Create your own website" link="/" />
+              <Button text="Connect Wallet" link="/" />
             </div>
           </MenuItem>
         </Menu>
         <div className="desktop">
-          <Button text="Create your own website" link="/" />
+          <Button text="Connect Wallet" link="/" />
         </div>
       </NavBar>
     </Section>
