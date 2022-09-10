92723b02212095d67478eaf5cf10be2bd6a441f1
Nadim <nahlenadim@gmail.com>
2022-09-10 14:14:34 +0300

    Frontend-React edit floating button

--- frontend-react/src/components/FloatingBtn.jsx
+++ frontend-react/src/components/FloatingBtn.jsx
@@ -2,22 +2,18 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
-import { useNavigate } from "react-router";
 
 const FloatingBtn = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/auth/chatbot");
-  };
+  const handleClick = () => {};
 
   return (
     <Box
       sx={{
         "& > :not(style)": {
-          p: 3,
+          p: 6,
           position: "absolute",
-          bottom: 10,
-          right: 10,
+          bottom: 20,
+          right: 30,
         },
       }}
       onClick={handleClick}
@@ -26,9 +22,9 @@ const FloatingBtn = () => {
         size="small"
         color="primary"
         aria-label="add"
-        style={{ backgroundColor: "red" }}
+        style={{ backgroundColor: "black" }}
       >
-        <SmartToyIcon style={{ fill: "white" }} />
+        <p className="floating-text">Create Your Own Website!</p>
       </Fab>
     </Box>
   );
