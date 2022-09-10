f0e707ce28a320f304b8cf06a6d0a07f16900277
Nadim <nahlenadim@gmail.com>
2022-09-10 11:03:13 +0300

    Frontend-React Link- AddHome

--- frontend-react/src/pages/HomeForm/HomeForm.jsx
+++ frontend-react/src/pages/HomeForm/HomeForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import axios from "../../api/axios";
 import "./HomeForm.css";
 
 const ADDHOME_URL = "/addhome";
@@ -9,23 +10,44 @@ const HomeForm = () => {
   const [logo, setLogo] = useState("");
   const [catchphrase, setCatchphrase] = useState("");
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(ADDHOME_URL, {
+        name,
+        logo,
+        catchphrase,
+      });
+      console.log(response);
+    } catch (err) {}
+  };
+
   return (
     <div className="container">
       <div className="contact-box">
         <div className="left"></div>
         <div className="right">
           <h2>Let's Create the Home Page!</h2>
-          <input type="text" className="field" placeholder="Website Name" />
+          <input
+            type="text"
+            className="field"
+            placeholder="Website Name"
+            onChange={(e) => setName(e.target.value)}
+          />
           <input
             type="text"
             className="field"
             placeholder="Website Abbreviation"
+            onChange={(e) => setLogo(e.target.value)}
           />
           <textarea
-            placeholder="Please Enter A Catchphrase!"
+            placeholder="Please Enter DADDHOME_URL Catchphrase!"
             className="field"
+            onChange={(e) => setCatchphrase(e.target.value)}
           ></textarea>
-          <button className="btn">Next</button>
+          <button className="btn" onClick={handleSubmit}>
+            Next
+          </button>
         </div>
       </div>
     </div>
